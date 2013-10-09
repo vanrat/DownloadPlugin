5dbcd8af60da2f5feb5592efea5024190e77fe74
vanrat <vanrat@happythingsgroup.com>
2013-10-09 19:09:32 +0700

    d

--- www/DownloadPlugin.js
+++ www/DownloadPlugin.js
@@ -6,3 +6,13 @@ var DownloadPlugin = {
      }
 }
 module.exports = DownloadPlugin;
+/*
+cordova.define("cordova/plugin/downloader", function (require, exports, module) {
+	var exec = require("cordova/exec");
+		module.exports = {
+			get: function (message, win, fail) {
+				exec(win, fail, "Downloader", "get", [message]);
+			}
+		};
+});
+*/
